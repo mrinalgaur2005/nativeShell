e497987eec7a445f4511081a921c37ffa82361d6
mrinalgaur2005 <mrinalgaur30@gmail.com>
2025-12-29 21:19:48 +0530

    Made the window Resizable

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
-
 #!/usr/bin/env bash
 set -e
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 gcc "$SCRIPT_DIR/src/main.c" \
-    $(sdl2-config --cflags --libs) \
+    $(pkg-config --cflags --libs sdl2 SDL2_image) \
     -o "$SCRIPT_DIR/main"
 
 "$SCRIPT_DIR/main"
--- src/main.c
+++ src/main.c
@@ -1,78 +1,113 @@
-#include <SDL2/SDL_error.h>
-#include <SDL2/SDL_events.h>
-#include <SDL2/SDL_render.h>
-#include <SDL2/SDL_stdinc.h>
-#include <SDL2/SDL_timer.h>
-#include <SDL2/SDL_video.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include <SDL2/SDL.h>
 #include <stdlib.h>
-#include <wchar.h>
 
 #define WINDOW_TITLE "Native Shell"
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
+#define IMAGE_FLAGS IMG_INIT_PNG
 
-struct Window{
+struct Window {
     SDL_Window *window;
     SDL_Renderer *renderer;
+    int width;
+    int height;
 };
-void game_cleanup(struct Window *Window ,int exit_status);
-bool sdl_init(struct Window *Window);
-int main(){
-    struct Window window = {
-        .window = NULL,
-        .renderer = NULL,
-    };
-    if(sdl_init(&window)){
-        game_cleanup(&window,EXIT_FAILURE);
-        printf("All BAD");
-        exit(1);
+
+void game_cleanup(struct Window *window, int exit_status);
+bool sdl_init(struct Window *window);
+
+int main(void) {
+    struct Window window = {0};
+
+    if (!sdl_init(&window)) {
+        game_cleanup(&window, EXIT_FAILURE);
     }
 
-    while (true){
-        SDL_Event event;
+    bool running = true;
+    SDL_Event event;
 
-        while(SDL_PollEvent(&event)){
+    while (running) {
+        while (SDL_PollEvent(&event)) {
             switch (event.type) {
+
                 case SDL_QUIT:
-                    game_cleanup(&window,EXIT_SUCCESS);
+                    running = false;
                     break;
-                default:
+
+                case SDL_WINDOWEVENT:
+                    if (event.window.event == SDL_WINDOWEVENT_RESIZED) {
+                        window.width  = event.window.data1;
+                        window.height = event.window.data2;
+
+                        SDL_RenderSetViewport(
+                            window.renderer,
+                            &(SDL_Rect){0, 0, window.width, window.height}
+                        );
+
+                        printf("Resized to %dx%d\n",
+                               window.width, window.height);
+                    }
                     break;
             }
         }
+
         SDL_RenderClear(window.renderer);
         SDL_RenderPresent(window.renderer);
-        SDL_Delay(16);
     }
-    game_cleanup(&window,EXIT_SUCCESS);
-    printf("All good");
+
+    game_cleanup(&window, EXIT_SUCCESS);
     return 0;
 }
 
-void game_cleanup(struct Window *Window,int exit_status){
-    SDL_DestroyRenderer(Window->renderer);
-    SDL_DestroyWindow(Window->window);
-    SDL_Quit();
-    exit(exit_status);
+bool sdl_init(struct Window *window) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        fprintf(stderr, "SDL init error: %s\n", SDL_GetError());
+        return false;
+    }
+
+    if ((IMG_Init(IMAGE_FLAGS) & IMAGE_FLAGS) != IMAGE_FLAGS) {
+        fprintf(stderr, "IMG init error: %s\n", IMG_GetError());
+        return false;
+    }
+
+    window->window = SDL_CreateWindow(
+        WINDOW_TITLE,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        SDL_WINDOW_RESIZABLE
+    );
+
+    if (!window->window) {
+        fprintf(stderr, "Window error: %s\n", SDL_GetError());
+        return false;
+    }
+
+    window->renderer = SDL_CreateRenderer(
+        window->window,
+        -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+    );
+
+    if (!window->renderer) {
+        fprintf(stderr, "Renderer error: %s\n", SDL_GetError());
+        return false;
+    }
+
+    window->width  = SCREEN_WIDTH;
+    window->height = SCREEN_HEIGHT;
+
+    return true;
 }
 
-bool sdl_init(struct Window *Window){
-    if(SDL_Init(SDL_INIT_EVERYTHING)!=0){
-        fprintf(stderr,"Error in init %s\n", SDL_GetError());
-        return true;
-    }
-    Window->window = SDL_CreateWindow(WINDOW_TITLE,SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,SCREEN_WIDTH,SCREEN_HEIGHT,0);
-    if(!Window->window){
-        fprintf(stderr, "Error creating window%s\n", SDL_GetError());
-        return true;
-    }
-    Window->renderer=SDL_CreateRenderer(Window->window,-1,0); 
-    if(!Window->renderer){
-        fprintf(stderr, "Error creating window%s\n", SDL_GetError());
-        return true;
-    }
-    return false;
+void game_cleanup(struct Window *window, int exit_status) {
+    if (window->renderer) SDL_DestroyRenderer(window->renderer);
+    if (window->window) SDL_DestroyWindow(window->window);
+    IMG_Quit();
+    SDL_Quit();
+    exit(exit_status);
 }
